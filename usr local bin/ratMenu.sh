15cccec9ee270640ae8013e60a752b499d18ec19
garnkmr@gmail.com <garnkmr@gmail.com>
2017-01-08 13:48:12 +0530

    updated ratpoisonrc for alt-tab, xsession file, updated ratmenu.

--- usr local bin/ratMenu.sh
+++ usr local bin/ratMenu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec ratmen -s dreary --background blue --foreground grey -label "Termite" termite "Chromium" chromium "File Explorer" dde-file-manager "Torrent" transmission-gtk "Brackets" brackets "Text Editor" mousepad
+exec ratmen -s dreary --background blue --foreground grey -label "Termite" termite "Chromium" chromium "File Explorer" spacefm "Torrent" transmission-gtk "Brackets" brackets "Text Editor" mousepad "Software Manager" pamac-manager  "~~~~Shutdown~~~~" shutdown
